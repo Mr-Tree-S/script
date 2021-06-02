0fd37f2a2dea2a8a1b1666f02d99c577591f86ab
ripper-s <ripper-s@ripper-s.com>
2021-06-02 15:31:39 +0800

    del something
    Signed-off-by: ripper-s <ripper-s@ripper-s.com>

--- python/Gopherus-master/install.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-pip2 install argparse
-pip2 install requests
-chmod +x gopherus.py
-ln -sf $(pwd)/gopherus.py /usr/local/bin/gopherus
--- python/a.py
+++
@@ -1,9 +0,0 @@
-from scapy.all import *
-
-flag = ""
-
-packets = rdpcap('capture.pcap')
-for packet in packets:
-    if UDP in packet and packet[UDP].dport == 22:
-        flag += chr(packet[UDP].sport - 5000)
-print(flag)
